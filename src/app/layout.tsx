02d189786c553872fa98a0928b3d1509e3a6472a
sivan wolberg <sivan@wolberg.pro>
2024-05-20 01:30:49 +0300

    small fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,9 +31,9 @@ export default function RootLayout({
               <div id="lColumn" >&nbsp;</div>
               <div id="rColumn" className="flex items-center gap-10 overflow-hidden text-sm font-semibold transition-all duration-200" style={{ height: " 20px" }}>
                 <div className="hidden md:block">
-                  <a href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app" target="_blank" rel="noreferrer" className="text-white hover:text-theme_airship_purple-200 cursor-pointer">Add your Business</a>
+                  <a href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app" target="_blank" rel="noreferrer" className="text-white hover:text-indigo-200 cursor-pointer">Add your Business</a>
                 </div>
-                <a href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app" target="_blank" rel="noreferrer"  className="text-white hover:text-theme_airship_purple-200 cursor-pointer">Out Mission</a>
+                <a href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app" target="_blank" rel="noreferrer"  className="text-white hover:text-indigo-200 cursor-pointer">Out Mission</a>
               </div>
             </div>
             <div className="w-full max-w-[1600px] pt-6 pb-36 lg:pb-10">
